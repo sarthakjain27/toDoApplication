239e45c762b92eec4d2e7e20669de673719d8982
Sarthak Jain <sarthakssj@gmail.com>
2019-09-23 11:43:35 -0700

    added connection string for mongodb cluster on atlas
    Trying to connect my webapp with a db instead of saving in local. Using cloud hosted mongodb atlas for hosting my db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,17 +4,16 @@ var data=[];
 var bodyparser=require('body-parser');
 var mongoose=require('mongoose');
 
-/*
-//Connect to the database
-//mongoose.connect('mongodb+srv://test:test@cluster0-vkmoj.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect('mongodb://sarthak:<insertYourPassword>@docdb-2019-09-23-07-02-06.cluster-cy3p0tyba1ct.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0',{useNewUrlParser: true, useUnifiedTopology: true});
+//Connect to the database
+mongoose.connect('mongodb+srv://sarthak:<password>@cluster0-0kdbg.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
+
 var todoSchema=new mongoose.Schema({
   item: String
 });
 
 var ToDo=mongoose.model('ToDo',todoSchema);
-*/
+
 
 /*
 Use below line if you want to by default want to have some items in db before application starts.
@@ -35,49 +34,49 @@ module.exports=function(app){
 
 app.get('/todo',function(req,res){
   //uncomment below line if not using DB
-  res.render('todo',{todos: data});
+  //res.render('todo',{todos: data});
 
   //If want to find a particular item. Give in the empty {} here like {item:'buy flowers'}
-/*
+
   ToDo.find({},function(err,data){
     if(err) throw err;
     res.render('todo',{todos: data});
   });
-*/
+
 });
 
 
 app.post('/todo',urlencoded,function(req,res){
 
-
+/*
     data.push(req.body);
     res.json(data);
 
+*/
 
-/*
   var addNew=ToDo(req.body).save(function(err,data){
     if(err) throw err;
     res.json(data);
   });
-*/
+
 });
 
 
 
 app.delete('/todo/:item',function(req,res){
 
-
+/*
   data=data.filter(function(each){
     return each.item.replace(/ /g,'-')!==req.params.item;
     });
     res.json(data);
+*/
 
-/*
     ToDo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
       if(err) throw err;
       res.json(data);
     });
-*/
+
 });
 
 };
